da44e94e530e40b5629da6a1de5aeeac0a121889
gportodev <gportodev@gmail.com>
2025-04-25 11:44:53 -0300

    feat: add list, navigation, icon, splash

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,40 @@
 import js from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginImport from 'eslint-plugin-import';
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-  },
+  js.configs.recommended,
+  eslint.configs.recommended,
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {
+      import: eslintPluginImport,
+      react: pluginReact,
+      'react-hooks': reactHooks,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/consistent-type-imports': 'off',
+      '@typescript-eslint/consistent-type-exports': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+  },
 ]);
